3925837e6a88797c2d754f45d53b14c12ff9d21f
Tiago Vinicius Souza dos Santos <tiagovinicius.souza@gmail.com>
2025-08-21 00:05:12 +0000

    primeira carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,30 @@
 #include <stdio.h>;
 
+
 int main() {
-    scanf("carta1"); 
-    scanf("")
-    
+    char Estado [50]; 
+    int População;
+    float Área; 
+    float Pib; 
+    int Númeropontoturistico; 
+
+    printf("Digite um estado: \n");
+    scanf("%s, Estado");
+
+    printf("Digite a polulação: \n");
+    scanf("%d, População");
+
+    printf("Digite a Área: \n"); 
+    scanf("%f, Área");
+
+    printf("Digite o número de pontos turistico: \n");
+    scanf("%d, número de pontos turisticos"); 
+
+
+
+
+
+
 
    
     return 0;
