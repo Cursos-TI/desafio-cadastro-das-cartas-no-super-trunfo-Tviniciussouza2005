b50f98dd01d91791fdbd11b1fe7bf7a1af171e10
Tiago Vinicius Souza dos Santos <tiagovinicius.souza@gmail.com>
2025-08-23 01:48:25 +0000

    continuação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,53 @@
 #include <stdio.h>;
 
 
-int main() {
+
+int main(){
    
 
 
-  
+    char Estado; 
+    char Codígo [50]; 
+    char Nomecidade [50]; 
+    int  População; 
+    float Pib; 
+    float Númerodepontosturísticos; 
 
+
+    printf("Digite um Estado \n");
+
+    scanf("%s, &Estado");
+
+
+    printf("Coloque um código no seu Estado \n"); 
+
+    scanf("%s, &Codigo"); 
+
+
+    printf("Digite o nome da cidade \n ");
+
+    scanf("%s, &Nomecidade");
+
+    
+    printf("Digite a população \n"); 
+
+    scanf("%d", &População);
+
+
+    printf("Digite o Pib \n");
+
+    scanf("%f, &Pib");
+
+
+    print("Quantos pontos turísticos tem? \n");
+
+    scanf("%f, &Númerodepontosturísticos");
 
 
 
 
    
+
+
     return 0;
 }
