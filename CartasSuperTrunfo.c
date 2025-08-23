8aca31c80b9db9ea7572a371c84772f4230a5c50
Tiago Vinicius Souza dos Santos <tiagovinicius.souza@gmail.com>
2025-08-23 00:37:54 +0000

    deleted

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,25 +2,10 @@
 
 
 int main() {
-    char Estado [50]; 
-    int População;
-    float Área; 
-    float Pib; 
-    int Númeropontoturistico; 
-
-    printf("Digite um estado: \n");
-    scanf("%s, Estado");
-
-    printf("Digite a polulação: \n");
-    scanf("%d, População");
-
-    printf("Digite a Área: \n"); 
-    scanf("%f, Área");
-
-    printf("Digite o número de pontos turistico: \n");
-    scanf("%d, número de pontos turisticos"); 
+   
 
 
+  
 
 
 
